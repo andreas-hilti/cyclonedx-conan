953a7f18269d7b95181ae020e8466150e3df9550
andreas hilti <andreas.hilti@bluewin.ch>
2023-05-14 20:20:36 +0200

    Support conan version 2 (only an initial draft)
    Signed-off-by: andreas hilti <andreas.hilti@bluewin.ch>

--- src/command.py
+++ src/command.py
@@ -23,9 +23,12 @@ import json
 import os.path
 import sys
 from uuid import uuid4
-from conans.client.conan_api import Conan, ProfileData
-from conans.client.command import Command as ConanCommand, OnceArgument, Extender, _add_common_install_arguments
-from conans.client.graph.graph import DepsGraph
+from conan.api.conan_api import ConanAPI
+from conan.cli.command import ConanCommand, OnceArgument
+from conan.cli.args import add_common_install_arguments, common_graph_args
+from conan.cli.commands.graph import graph_info
+from conans.model.recipe_ref import RecipeReference
+from conans.client.graph.graph import DepsGraph, Node
 from conans.errors import ConanMigrationError, ConanException
 from packageurl import PackageURL
 
@@ -41,58 +44,59 @@ class CycloneDXCommand:
     def get_arg_parser() -> argparse.ArgumentParser:
         parser = argparse.ArgumentParser(description='CycloneDX SBOM Generator')
 
-        parser.add_argument("path_or_reference", help="Path to a folder containing a recipe"
-                            " (conanfile.py or conanfile.txt) or to a recipe file. e.g., "
-                            "./my_project/conanfile.txt. It could also be a reference")
         parser.add_argument("-if", "--install-folder", action=OnceArgument,
                             help="local folder containing the conaninfo.txt and conanbuildinfo.txt "
                             "files (from a previous conan install execution). Defaulted to "
                             "current folder, unless --profile, -s or -o is specified. If you "
                             "specify both install-folder and any setting/option "
                             "it will raise an error.")
-        dry_build_help = ("Apply the --build argument to output the information, "
-                          "as it would be done by the install command")
-        parser.add_argument("-db", "--dry-build", action=Extender, nargs="?", help=dry_build_help)
-        build_help = ("Given a build policy, return an ordered list of packages that would be built"
-                      " from sources during the install command")
-
-        update_help = "Will check if updates of the dependencies exist in the remotes " \
-                      "(a new version that satisfies a version range, a new revision or a newer " \
-                      "recipe if not using revisions)."
-        _add_common_install_arguments(parser, update_help=update_help, build_help=build_help)
+        def remove_argument(parser, argument):
+            for action in parser._actions:
+                option_strings = action.option_strings
+                if (option_strings and option_strings[0] == argument) or action.dest == argument:
+                    parser._remove_action(action)
+                    break
+            for group in parser._action_groups:
+                for group_action in group._group_actions:
+                    option_strings = group_action.option_strings
+                    if (option_strings and option_strings[0] == argument) or group_action.dest == argument:
+                        group._group_actions.remove(group_action)
+                        break
+        common_graph_args(parser)
+        remove_argument(parser, "requires")
+        remove_argument(parser, "--tool-requires")
 
         return parser
 
     def execute(self):
         try:
-            conan_api, _, _ = Conan.factory()
+            conan_api = ConanAPI()
         except ConanMigrationError:  # Error migrating
             sys.exit(1)
         except ConanException as e:
             sys.stderr.write("Error in Conan initialization: {}".format(e))
             sys.exit(1)
-        conan_command = ConanCommand(conan_api)
+        cwd = os.getcwd()
+        args = self._arguments
 
-        profile_build = ProfileData(profiles=self._arguments.profile_build,
-                                    settings=self._arguments.settings_build,
-                                    options=self._arguments.options_build,
-                                    env=self._arguments.env_build,
-                                    conf=self._arguments.conf_build)
-        data = conan_command._conan.info(
-            self._arguments.path_or_reference,
-            remote_name=self._arguments.remote,
-            settings=self._arguments.settings_host,
-            options=self._arguments.options_host,
-            env=self._arguments.env_host,
-            profile_names=self._arguments.profile_host,
-            conf=self._arguments.conf_host,
-            profile_build=profile_build,
-            update=self._arguments.update,
-            install_folder=self._arguments.install_folder,
-            build=self._arguments.dry_build,
-            lockfile=self._arguments.lockfile)
+        if not args.path:
+            sys.stderr.write("Error: You need to provide the path.")
+            sys.exit(1)
 
-        deps_graph: DepsGraph = data[0]
+        path = conan_api.local.get_conanfile_path(args.path, cwd, py=None)
+
+        remotes = conan_api.remotes.list(args.remote) if not args.no_remote else []
+        lockfile = conan_api.lockfile.get_lockfile(lockfile=args.lockfile,
+                                                conanfile_path=path,
+                                                cwd=cwd,
+                                                partial=args.lockfile_partial)
+        profile_host, profile_build = conan_api.profiles.get_profiles_from_args(args)
+
+
+        deps_graph: DepsGraph = conan_api.graph.load_graph_consumer(path, args.name, args.version,
+                                                            args.user, args.channel,
+                                                            profile_host, profile_build, lockfile,
+                                                            remotes, args.build, args.update)
 
         bom = {
             "bomFormat": "CycloneDX",
@@ -110,8 +114,9 @@ class CycloneDXCommand:
             'components': [],
             'dependencies': [],
         }
+        node:Node
         for node in deps_graph.nodes:
-            if node.ref is None:
+            if not node.inverse_neighbors():
                 # top level component
                 bom['metadata']['component']['name'] = os.path.basename(os.path.dirname(node.path))
                 bom['metadata']['component']['bom-ref'] = bom['metadata']['component']['name'] + '@' + bom['metadata']['component']['version']
@@ -129,7 +134,7 @@ class CycloneDXCommand:
                     'bom-ref': str(purl),
                     'type': 'library',
                     'name': node.ref.name,
-                    'version': node.ref.version,
+                    'version': str(node.ref.version),
                     'purl': str(purl),
                 }
                 if node.ref.user:
@@ -147,13 +152,13 @@ class CycloneDXCommand:
         print(json.dumps(bom, indent=2))
 
 
-def get_purl(remote, ref):
+def get_purl(remote, ref: RecipeReference):
     qualifiers = {
         'repository_url': 'localhost' if remote is None else remote.url,
     }
     if ref.user:
         qualifiers['channel'] = ref.channel
-    purl = PackageURL(type='conan', namespace=ref.user, name=ref.name, version=ref.version, qualifiers=qualifiers)
+    purl = PackageURL(type='conan', namespace=ref.user, name=ref.name, version=str(ref.version), qualifiers=qualifiers)
     return purl
 
 
